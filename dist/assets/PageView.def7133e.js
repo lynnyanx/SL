e2aedfe701d31d13a2001e77c0c4f1beb98df851
lynnyanx <145647157+lynnyanx@users.noreply.github.com>
2024-01-24 13:31:22 +0800

    Add files via upload

---
+++ dist/assets/PageView.def7133e.js
@@ -0,0 +1,1 @@
+import{_ as o,r as t,c as r,a as c,o as n}from"./index.f5f784a8.js";const s={},a={id:"container"};function _(i,d){const e=t("router-view");return n(),r("div",a,[c(e)])}const m=o(s,[["render",_]]);export{m as default};
